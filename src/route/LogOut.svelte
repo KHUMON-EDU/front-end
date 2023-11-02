5548da995407b72e1bcb0f289f882001e6b875dd
Sihwan Kim <vkn123@naver.com>
2023-11-02 15:00:57 +0900

    feat: 메인 페이지 테이블 기능

--- src/route/LogOut.svelte
+++ src/route/LogOut.svelte
@@ -2,7 +2,7 @@
     import LoginPage from "./LoginPage.svelte";
     import axios from 'axios'
     let header = sessionStorage.getItem("access-code")
-    axios.get("http://localhost:8080/logout",{
+    axios.get("http://localhost:8080/api/auth/logout",{
         headers:{"Authorization": header},
       }).then(
         response => console.log(response)
--- src/route/MainPage.svelte
+++ src/route/MainPage.svelte
@@ -1,6 +1,6 @@
 <script>
     import LoginPage from "./LoginPage.svelte";
-
+    import { Button,Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
 
     let id = sessionStorage.getItem("id")
     let accessCode = sessionStorage.getItem("access-code")
@@ -9,28 +9,82 @@
 </script>
 
 {#if accessCode}
-<div style="align-items:end;">
-    <p style="color: black; align-items:end">{nickName} 님 안녕하세요.      <a href="#/logout">로그아웃</a></p>
-</div>
-
-<div style="align-items: center; display:flex">
-
-    <div style="height:max-content; margin-bottom:10%">
-    <img src="images/khu_mon_logo.png" alt="logo" width="100%" style=" margin-bottom: 5%;float: left; width:100%; height:auto; align-items: center; display: flex;" >
+<div class="flex">
+    <div class="w-30">
+        <img src="images/khu_mon_logo.png" alt="logo" width="50%" style=" margin-bottom: 5%;">
     </div>
+    <div class=" align-bottom">
+        <p style="color: black; align-items:end">{nickName} 님 안녕하세요.      <a href="#/logout">로그아웃</a></p>
+      </div>
 
-    <div style="float: left; width:50%">
-    <a href="#/upload" style="width: fit-content; height:fit-content">
-    <img src="images/add_study_button.png" alt="logo" width="100%" style=" margin-bottom: 5%;">
-    </a>
 
-    <a  href = "#/note">
-        <img src="images/note_button.png" alt="logo" width="100%" style=" margin-bottom: 5%;">
-    </a>
-    </div>
 
 
 </div>
+
+<div style="width: 100%;">
+  <Button>추가</Button>
+<Table striped={true} hoverable={true} shadow ={true}>
+    <TableHead>
+      <TableHeadCell>-</TableHeadCell>
+      <TableHeadCell>노트 이름</TableHeadCell>
+      <TableHeadCell>파일 형태</TableHeadCell>
+      <TableHeadCell>문자 개수</TableHeadCell>
+      <TableHeadCell>만든 날짜</TableHeadCell>
+      <TableHeadCell>수정 날짜</TableHeadCell>
+      <TableHeadCell>
+        <span class="sr-only">보러가기</span>
+      </TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+      <TableBodyRow>
+        <TableBodyCell><Checkbox></Checkbox></TableBodyCell>
+        <TableBodyCell>인공지능 프로그래밍</TableBodyCell>
+        <TableBodyCell>MP4</TableBodyCell>
+        <TableBodyCell>5개</TableBodyCell>
+        <TableBodyCell>2023.01.03</TableBodyCell>
+        <TableBodyCell>2023.05.06</TableBodyCell>
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-primary-600 hover:no-underlinee dark:text-primary-500">보러가기</a>
+        </TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell><Checkbox></Checkbox></TableBodyCell>
+        <TableBodyCell>인공지능 프로그래밍</TableBodyCell>
+        <TableBodyCell>MP4</TableBodyCell>
+        <TableBodyCell>5개</TableBodyCell>
+        <TableBodyCell>2023.01.03</TableBodyCell>
+        <TableBodyCell>2023.05.06</TableBodyCell>
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-primary-600 hover:no-underline dark:text-primary-500">보러가기</a>
+        </TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell><Checkbox></Checkbox></TableBodyCell>
+        <TableBodyCell>인공지능 프로그래밍</TableBodyCell>
+        <TableBodyCell>MP4</TableBodyCell>
+        <TableBodyCell>5개</TableBodyCell>
+        <TableBodyCell>2023.01.03</TableBodyCell>
+        <TableBodyCell>2023.05.06</TableBodyCell>
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-primary-600 hover:no-underline dark:text-primary-500">보러가기</a>
+        </TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell><Checkbox></Checkbox></TableBodyCell>
+        <TableBodyCell>인공지능 프로그래밍</TableBodyCell>
+        <TableBodyCell>MP4</TableBodyCell>
+        <TableBodyCell>5개</TableBodyCell>
+        <TableBodyCell>2023.01.03</TableBodyCell>
+        <TableBodyCell>2023.05.06</TableBodyCell>
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-primary-600 hover:no-underline dark:text-primary-500">보러가기</a>
+        </TableBodyCell>
+      </TableBodyRow>
+    </TableBody>
+  </Table>
+</div>
+
 
 {:else}
     <LoginPage />
